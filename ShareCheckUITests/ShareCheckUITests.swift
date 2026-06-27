de1f34407927063fd590a446ac805ee9f613bcaa
ishiishikou <kou.ishi@gmail.com>
2026-06-27 17:38:03 +0900

    Pass UI testing launch argument

--- ShareCheckUITests/ShareCheckUITests.swift
+++ ShareCheckUITests/ShareCheckUITests.swift
@@ -3,6 +3,7 @@ import XCTest
 final class ShareCheckUITests: XCTestCase {
     func testAppLaunches() {
         let app = XCUIApplication()
+        app.launchArguments.append("--ui-testing")
         app.launch()
         XCTAssertTrue(app.tabBars.firstMatch.waitForExistence(timeout: 5))
     }
